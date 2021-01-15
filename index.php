1ac76a0f0d5647bd40e815965907ee3d0fb7588a
Justinzsamp <fullyz1532@gmail.com>
2021-01-15 21:22:51 +0700

    เพิ่ม Slide Show

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+    />
+    <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/main.css?v=<?=time();?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body>
+    <div class="main-container">
+        <div id="navbar">
+            <a href="#EVENT" class="animate__animated animate__heartBeat">CONTACT</a>
+            <a href="#EVENT" class="animate__animated animate__heartBeat">ราคาถ่ายภาพต่างๆ</a>
+            <a href="#EVENT" class="animate__animated animate__heartBeat">EVENT</a>
+            <a href="#WEDDING" class="animate__animated animate__heartBeat">WEDDING</a>
+            <a href="#PREWEDDING" class="animate__animated animate__heartBeat">PRE WEDDING</a>
+            <a href="#GRADUATION" class="animate__animated animate__heartBeat">GRADUATION</a>
+            <a href="#HOME" class="animate__animated animate__heartBeat">HOME</a>
+            <div class="navbar-left">
+                <a style="margin-left: 20px;" href="#HOME" class="animate__animated animate__heartBeat">รับจ้างถ่ายรูปภาพ</a>
+            </div>
+        </div>
+        <div class="banner-head-top" id="home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center" data-sr='enter top'>
+                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#myCarousel" data-slide-to="1"></li>
+                                <li data-target="#myCarousel" data-slide-to="2"></li>
+                                <li data-target="#myCarousel" data-slide-to="3"></li>
+                                <li data-target="#myCarousel" data-slide-to="4"></li>
+                                <li data-target="#myCarousel" data-slide-to="5"></li>
+                                <li data-target="#myCarousel" data-slide-to="6"></li>
+                                <li data-target="#myCarousel" data-slide-to="7"></li>
+                                <li data-target="#myCarousel" data-slide-to="8"></li>
+                                <li data-target="#myCarousel" data-slide-to="9"></li>
+                                <li data-target="#myCarousel" data-slide-to="10"></li>
+                                <li data-target="#myCarousel" data-slide-to="11"></li>
+                                <li data-target="#myCarousel" data-slide-to="12"></li>
+                            </ol>
+
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                    <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+
+                                <div class="item">
+                                <img src="img/1.jpg" alt="">
+                                </div>
+                            </div>
+                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
