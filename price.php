074a06c123d6bed2095e399405e252560fd68ad5
JeTK77 <thinnaphat12331@gmail.com>
2021-01-16 22:52:40 +0700

    Price

--- price.php
+++ price.php
@@ -18,17 +18,240 @@
 <body>
     <div class="main-container" >
         <div id="navbar">
-            <a href="login.php" class="animate__animated animate__heartBeat">เข้าสู่ระบบ</a>
             <a href="contact.php" class="animate__animated animate__heartBeat">CONTACT</a>
+            <a href="work.php" class="animate__animated animate__heartBeat">ผลงาน</a>
             <a href="price.php" class="animate__animated animate__heartBeat">ราคาถ่ายภาพต่างๆ</a>
             <a href="#EVENT" class="animate__animated animate__heartBeat">EVENT</a>
-            <a href="wedding.php" class="animate__animated animate__heartBeat">WEDDING</a>
-            <a href="prewedding.php" class="animate__animated animate__heartBeat">PRE WEDDING</a>
-            <a href="graduation.php" class="animate__animated animate__heartBeat">GRADUATION</a>
+            <a href="#WEDDING" class="animate__animated animate__heartBeat">WEDDING</a>
+            <a href="#PREWEDDING" class="animate__animated animate__heartBeat">PRE WEDDING</a>
+            <a href="#GRADUATION" class="animate__animated animate__heartBeat">GRADUATION</a>
             <a href="index.php" class="animate__animated animate__heartBeat">HOME</a>
             <div class="navbar-left">
                 <a style="margin-left: 20px;" href="#HOME" class="animate__animated animate__heartBeat">Photographer</a>
             </div>
+        </div>
+        <div class="mainbody">
+            <h1>Price List</h1>
+            <br>
+            <h3>ค่าบริการถ่ายภาพพรีเวดดิ้ง, งานแต่งและงานหมั้น, งานรับปริญญา, งานอีเว้นท์และปาร์ตี้</h3>
+            <br>
+            <br>
+            <div class="bodybutton">
+                <button class="facebookbutton">
+                    <a href="#">
+                        <i class="fa fa-facebook"></i>
+                        Facebook
+                    </a>        
+                </button>
+                <button class="linebutton">
+                    <a href="#">
+                        <img src="img/Icon-Line.png">Line
+                    </a>   
+                </button>
+            </div>
+            <br>
+            <br>              
+            <h3>Line ID: @bboyphoto ● 099-999-9998</h3>
+
+            <div class="linear">
+                <img src="img/2.png">
+            </div>   
+
+            <div class="congrad">
+                <img src="img/Graduation/grad2.jpg">
+                <h3>
+                    รับปริญญา Graduation
+                </h3>
+                <h4>
+                    รับปริญญา วันซ้อม/รับจริง (วันที่ต้องมีการเข้าหอประชุม)
+                </h4>
+                <h5 class="h51">
+                    เต็มวัน 4,500 บาท (เช้าก่อนเข้าหอประชุม จนถึงเย็นหลังออกจากหอประชุม)
+                </h5>
+                <h5 class="h52">
+                    ครึ่งวัน 3,500 บาท (เช้าหรือเย็น ก่อนเข้า-ออกจากห้องประชุม ประมาณ 4 ชั่วโมง)
+                </h5>
+                <h5 class="h53">
+                    (***สำหรับวัน Photo day ที่ไม่มีการเข้าหอประชุม นับเต็มวัน 8 ชั่วโมงครับ ส่วนครึ่งวันประมาณ 4 ชั่วโมง)
+                </h5>
+                <h6 class ="h61">
+                    สิ่งที่ได้รับหลังจากเสร็จงาน
+                </h6>
+                <h6 class ="h62">
+                    1.อัดภาพขนาด 4x6 จำนวน 40 รูป
+                </h6>
+                <h6 class ="h63">
+                    2.อัดภาพขนาด 8x12 จำนวน 1 รูป
+                </h6>
+                <h6 class ="h64">
+                    3.อัดภาพโปสเตอร์ขนาด 10x15 จำนวน 1 รูป
+                </h6>
+                <h6 class ="h65">
+                    4.ได้รับไฟล์ภาพที่ปรับแสงสีทุกใบที่ถ่ายในวันนั้น
+                </h6>
+                <h6 class ="h66">
+                    5.ลงแผ่น DVD พร้อมสกีนปกสวยงาม
+                </h6>
+                <h6 class ="h71">
+                    นอกรอบ หรือวันที่ไม่มีการรับปริญญา (ราคาเดียว)
+                </h6>
+                <h6 class ="h72">
+                    ครึ่งวัน 3,500 บาท (4 ชั่วโมง)
+                </h6>
+                <h6 class ="h73">
+                    เต็มวัน 4,500 บาท (8 ชั่วโมง)
+                </h6>
+                <h6 class ="h81">
+                    นอกรอบ แบบมาเป็นกลุ่ม
+                </h6>
+                <h6 class ="h82">
+                    คิดคนแรก 4,000 บาท คนต่อไปเพิ่มหัวละ 800 บาท
+                </h6>
+                <h6 class ="h83">
+                    อัดภาพขนาด 4x6 คนละ 20 รูป
+                </h6>
+                <h6 class ="h84">
+                    อัดภาพโปสเตอร์ขนาด 10x15 คนละ 1 รูป
+                </h6>
+                <h6 class ="h85">
+                    ลงแผ่น DVD พร้อมสกีนปกสวยงาม
+                </h6>
+                <h6 class ="h91">
+                    ถ่ายไม่จำกัดจำนวน
+                </h6>
+                <h6 class ="h92">
+                    แต่งภาพแสงสีปกติใสๆ ให้ทุกใบ และทำการรีทัชพิเศษ เช่น ลบสิ้ว ริ้วรอย
+                </h6>
+                <h6 class ="h93">
+                    ระยะเวลาส่งงาน 2-4 สัปดาห์ หลังถ่ายภาพ
+                </h6>
+                <h6 class ="h94">
+                    มัดจำ 1,000 บาท เพื่อจองวันครับผม
+                </h6>
+                <h6 class ="h95">
+                    !! ยังไม่รวมค่าเดินทางและที่พัก(กรณีต้องค้างคืน)
+                </h6>
+            </div> 
+            <div class="prewedding">
+                <img src="img/Pre wedding/pre6.jpg">
+                <h6 class="p">
+                    พรีเวดดิ้ง Pre Wedding
+                </h6>
+                <h6 class="p1">
+                    Package 4,500 (4 ชั่วโมง)
+                </h6>
+                <h6 class="p2">
+                    Package 5,500 (8 ชั่วโมง)
+                </h6>
+                <h6 class="p3">
+                    บ่าว-สาว เตรียมชุดมาเอง ไม่จำกัดจำนวนชุด
+                </h6>
+                <h6 class="p4">
+                    ได้รับไฟล์ ทั้งหมด โดยไม่ต้องซื้อเพิ่ม
+                </h6>
+                <h6 class="p5">
+                    แต่งภาพแสงสีปกติใสๆ ให้ทุกใบ แต่งพิเศษให้อีกอย่างน้อย 30 รูป
+                </h6>
+                <h6 class="p6">
+                    ส่งงาน DVD BOXSET พร้อมสกรีนหน้าปกสวยงาม
+                </h6>
+            </div>   
+            <div class="wedding">
+                <img src="img/Wedding/wed4.jpg">
+                <h6 class="w">
+                    พิธีแต่งงาน Wedding Ceremony
+                </h6>
+                <h6 class="w1">
+                    งานพิธีเช้า
+                </h6>
+                <h6 class="w2">
+                    ภาพนิ่ง 1 คน 5,000 บาท
+                </h6>
+                <h6 class="w3">
+                    ภาพนิ่ง 2 คน 10,000 บาท
+                </h6>
+                <h6 class="w4">
+                    งานพิธีเย็น
+                </h6>
+                <h6 class="w5">
+                    ภาพนิ่ง 1 คน และผู้ช่วย 1 คน 6,000 บาท
+                </h6>
+                <h6 class="w6">
+                    ภาพนิ่ง 2 คน และผู้ช่วย 1 คน 11,000 บาท
+                </h6>
+                <h6 class="w7">
+                    งานพิธีเช้า + เลี้ยงกลางวัน
+                </h6>
+                <h6 class="w8">
+                    ภาพนิ่ง 1 คน และผู้ช่วย 1 คน 8,000 บาท
+                </h6>
+                <h6 class="w9">
+                    ภาพนิ่ง 2 คน และผู้ช่วย 1 คน 12,000 บาท
+                </h6>
+                <h6 class="w10">
+                    งานพิธีเช้า + งานพิธีเย็น
+                </h6>
+                <h6 class="w11">
+                    ภาพนิ่ง 1 คน และผู้ช่วย 1 คน 11,000 บาท
+                </h6>
+                <h6 class="w12">
+                    ภาพนิ่ง 2 คน และผู้ช่วย 1 คน 16,500 บาท
+                </h6>
+                <h6 class="w13">
+                    ราคาทั้งหมดมาพร้อมชุดไฟสตูดิโอหน้า Backdrop 2 ดวง ไฟส่องสว่างหน้างาน และไฟนำเข้างานเลี้ยงฉลอง
+                </h6>
+                <h6 class="w14">
+                    ถ่ายไม่จำกัดจำนวน และให้ไฟล์ภาพทั้งหมด
+                </h6>
+                <h6 class="w15">
+                    แต่งภาพแสงสีปกติใสๆ ให้ทุกใบ
+                </h6>
+                <h6 class="w16">
+                    ระยะเวลาส่งงาน 2-4 สัปดาห์ หลังถ่ายภาพ ส่งงานเป็น DVD ทาง EMS
+                </h6>
+                <h6 class="w17">
+                    มัดจำเพื่อจองวันครับผม
+                </h6>
+                <h6 class="w18">
+                    อัดภาพขนาด 4x6 จำนวน 100 ภาพ
+                </h6>
+                <h6 class="w19">
+                    ใน กทม ไม่คิดค่าเดินทางและที่พัก
+                </h6>
+            </div>
+            <div class="event">
+                <img src="img/Event+/2.jpg">
+                <h6 class="e">
+                    งาน Event
+                </h6>
+                <h6 class="e1">
+                    ครึ่งวัน 4,000 ( 4 ชม.)
+                </h6>
+                <h6 class="e2">
+                    เต็มวัน 5,000 ( 8 ชม.)
+                </h6>
+                <h6 class="e3">
+                    ได้รับไฟล์ภาพที่ปรับแสงสีทุกใบที่ถ่ายในวันนั้น
+                </h6>
+                <h6 class="e4">
+                    ลงแผ่น DVD พร้อมสกีนปกสวยงาม
+                </h6>
+                <h6 class="e5">
+                    !! รายละเอียดการติดต่อ และชำระเงินจอง
+                </h6>
+                <h6 class="e6">
+                    โทรตรวจสอบและคุยรายละเอียดงานก่อนได้ที่ 092-314-3010 (บอย)
+                </h6>
+                <h6 class="e7">
+                    facebook:<a href="https://www.facebook.com/amnad.sanrin"> https://www.facebook.com/amnad.sanrin</a>
+                </h6>
+                <h6 class="e8">
+                    Line ID: @bboyphoto
+                </h6>
+                <h6 class="e9">
+                    เพื่อตกลงวันที่ต้องการถ่ายภาพ
+                </h6>
+            </div>         
         </div>
     </div>
 </div>
