a18f28e1829ac0699a4664c22bbb06677453e8a8
Justinzsamp <fullyz1532@gmail.com>
2021-05-17 03:14:24 +0700

    แก้ calendar

---
+++ calendar.php
@@ -0,0 +1,13 @@
+<monthly>
+	<event>
+		<id>1</id>
+		<name>XML Events</name>
+		<startdate><?php echo $row['Year']; ?>-<?php echo $row['Month']-1; ?>-<?php echo $row['Day']; ?></startdate>
+		<enddate><?php echo $row['Year']; ?>-<?php echo $row['Month']-1; ?>-<?php echo $row['Day']; ?></enddate>
+		<starttime>12:00</starttime>
+		<endtime>2:00</endtime>
+		<color>#ffb128</color>
+		<url></url>
+	</event>
+
+</monthly>
